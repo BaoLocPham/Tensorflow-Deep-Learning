a194ac930bd58ff8201f97a3007602060edc40d2
Bao Loc Pham <67360122+BaoLocPham@users.noreply.github.com>
2021-08-12 22:06:16 +0700

    update make confusion matrix

--- Utils/helper_functions.py
+++ Utils/helper_functions.py
@@ -91,7 +91,11 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
   # Make x-axis labels appear on bottom
   ax.xaxis.set_label_position("bottom")
   ax.xaxis.tick_bottom()
-
+  
+  # changed
+  plt.xticks(rotation=70, fontsize=text_size)
+  plt.yticks(fontsize=text_size)
+  
   # Set the threshold for different colors
   threshold = (cm.max() + cm.min()) / 2.
 
